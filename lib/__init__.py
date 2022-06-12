fd1bc40b40781d1298a4694b57cef56c2aa1724b
Christos Tzelepis <null.geppetto@gmail.com>
2022-06-12 21:28:53 +0100

    Minor edits

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,9 +1,9 @@
 from .aux import create_exp_dir, PromptFeatures, update_stdout, update_progress, sample_z, \
     create_summarizing_gif, tensor2image
 from .config import SEMANTIC_DIPOLES_CORPORA
+from .config import GENFORCE, GENFORCE_MODELS, STYLEGAN_LAYERS
+from .config import SFD, ARCFACE, FAIRFACE, HOPENET, AUDET, CELEBA_ATTRIBUTES
 from .config import ContraCLIP_models
-from .config import ARCFACE
-from .config import GENFORCE, GENFORCE_MODELS
 from .support_sets import SupportSets
 from .trainer import Trainer
 from .data import PathImages
