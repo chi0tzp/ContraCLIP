78fff2f379f8dc0cfd414b2de197fba44145a189
Christos Tzelepis <null.geppetto@gmail.com>
2022-06-16 11:57:12 +0100

    Add aux scripts and latent codes pool for StyleGAN2 on AFHQ Dogs

---
+++ scripts/eval/stylegan2_afhqdog512_w+.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# ===== Configuration =====
+pool="stylegan2_afhqdog512-16"
+# -------------------------
+eps=0.15
+shift_leap=5
+batch_size=20
+# =========================
+
+# Define shift steps
+declare -a SHIFT_STEPS=(120)
+
+# Define experiment directories list
+declare -a EXPERIMENTS=(
+                        "experiments/complete/ContraCLIP_stylegan2_afhqdog512-W+-K4-D128-eps0.1_0.2-nonlinear_beta-0.75-contrastive_1.0-10000-dogs"
+                        )
+
+for shift_s in "${SHIFT_STEPS[@]}"
+do
+  for exp in "${EXPERIMENTS[@]}"
+  do
+    python traverse_latent_space.py -v --gif \
+                                    --exp="${exp}" \
+                                    --pool="${pool}" \
+                                    --eps=${eps} \
+                                    --shift-steps="${shift_s}" \
+                                    --shift-leap=${shift_leap} \
+                                    --batch-size=${batch_size} \
+                                    --img-size=512 \
+                                    --gif \
+                                    --gif-size=256 \
+                                    --strip \
+                                    --strip-size=256
+  done
+done
