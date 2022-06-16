a9014e724b46129aa5e74e6456c5deef608f8c49
Christos Tzelepis <null.geppetto@gmail.com>
2022-06-16 12:49:06 +0100

    Add aux scripts and latent codes pool for StyleGAN2 on Cars

--- scripts/eval/stylegan2_car512_w.sh
+++ scripts/eval/stylegan2_car512_w.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # ===== Configuration =====
-pool="stylegan2_car512-8"
+pool="stylegan2_car512-6"
 # -------------------------
 eps=0.15
 shift_leap=3
-batch_size=6
+batch_size=20
 # =========================
 
 # Define shift steps
-declare -a SHIFT_STEPS=(32 48 64)
+declare -a SHIFT_STEPS=(80)
 
 # Define experiment directories list
 declare -a EXPERIMENTS=(
-                        "experiments/complete/ContraCLIP_stylegan2_car512-W-K3-D128-eps0.1_0.2-nonlinear_beta-0.75-contrastive_0.5-cars"
+                        "experiments/complete/ContraCLIP_stylegan2_car512-W-K3-D128-eps0.1_0.2-nonlinear_beta-0.75-contrastive_1.0-10000-cars"
                         )
 
 for shift_s in "${SHIFT_STEPS[@]}"
@@ -26,6 +26,11 @@ do
                                     --eps=${eps} \
                                     --shift-steps="${shift_s}" \
                                     --shift-leap=${shift_leap} \
-                                    --batch-size=${batch_size}
+                                    --batch-size=${batch_size} \
+                                    --img-size=512 \
+                                    --gif \
+                                    --gif-size=256 \
+                                    --strip \
+                                    --strip-size=256
   done
 done
