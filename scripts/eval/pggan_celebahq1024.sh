d2ab6ab395b099f348ee57c81c922daceaf041c3
Christos Tzelepis <null.geppetto@gmail.com>
2022-06-16 13:12:57 +0100

    Add aux scripts and latent codes pool for ProgGAN (CelebA-HQ)

--- scripts/eval/pggan_celebahq1024.sh
+++ scripts/eval/pggan_celebahq1024.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # ===== Configuration =====
-pool="pggan_celebahq1024-4"
+pool="pggan_celebahq1024-8"
 # -------------------------
 eps=0.15
 shift_leap=3
-batch_size=6
+batch_size=10
 # =========================
 
 # Define shift steps
-declare -a SHIFT_STEPS=(48 64 72)
+declare -a SHIFT_STEPS=(50)
+
 
 # Define experiment directories list
 declare -a EXPERIMENTS=(
-                        "experiments/complete/ContraCLIP_pggan_celebahq1024-Z-K11-D128-eps0.1_0.2-nonlinear_beta-0.75-contrastive_0.5-expressions"
-                        "experiments/complete/ContraCLIP_pggan_celebahq1024-Z-K9-D128-eps0.1_0.2-nonlinear_beta-0.75-contrastive_0.5-attributes"
+                        "experiments/complete/ContraCLIP_pggan_celebahq1024-Z-K8-D128-eps0.1_0.2-nonlinear_beta-0.75-contrastive_1.0-10000-attributes"
+                        "experiments/complete/ContraCLIP_pggan_celebahq1024-Z-K11-D128-eps0.1_0.2-nonlinear_beta-0.75-contrastive_1.0-10000-expressions"
                         )
 
 for shift_s in "${SHIFT_STEPS[@]}"
@@ -27,6 +28,11 @@ do
                                     --eps=${eps} \
                                     --shift-steps="${shift_s}" \
                                     --shift-leap=${shift_leap} \
-                                    --batch-size=${batch_size}
+                                    --batch-size=${batch_size} \
+                                    --img-size=512 \
+                                    --gif \
+                                    --gif-size=256 \
+                                    --strip \
+                                    --strip-size=256
   done
 done
