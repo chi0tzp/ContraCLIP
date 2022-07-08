436c65b69103748cc92f7efbab32e37d33f19478
Christos Tzelepis <null.geppetto@gmail.com>
2022-07-09 00:55:49 +0100

    Remove scripts temporarily

---
+++ scripts/compare/stylegan2_ffhq1024_expressions_contrastive-vs-cossim.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ==================================================================================================================== #
+declare -a EXPERIMENTAL_RESULTS=(
+          "experiments/complete/ContraCLIP_stylegan2_ffhq1024-W+-K21-D64-lss_beta_0.5-eps0.1_0.2-nonlinear_css_beta_0.5-contrastive_0.07-20000-expressions/results/stylegan2_ffhq1024-8/200_0.15_30.0/"
+          "experiments/complete/ContraCLIP_stylegan2_ffhq1024-W+-K21-D64-lss_beta_0.5-eps0.1_0.2-nonlinear_css_beta_0.5-cossim-20000-expressions/results/stylegan2_ffhq1024-8/200_0.15_30.0/"
+          )
+# ==================================================================================================================== #
+
+e=""
+for exp in "${EXPERIMENTAL_RESULTS[@]}"
+do
+  e+="${exp} "
+done
+
+filename=$(basename "$0")
+comparison_name="${filename%.*}"
+python compare_experiments.py -n "${comparison_name}" -e $e
--- scripts/train/dev.sh
+++ scripts/train/dev.sh
@@ -6,6 +6,7 @@
 # ======== GAN Type / Corpus ======== #
 gan="stylegan2_ffhq1024"
 stylegan_space="S"
+stylegan_layer=11
 corpus="expressions3"
 
 # ==== Latent Support Sets (LSS) ==== #
@@ -15,40 +16,28 @@ max_shift_magnitude=0.2
 lss_beta=0.5
 
 # ==== Corpus Support Sets (CSS) ==== #
-linear=false
-styleclip_like=false
 loss="cossim"
 temperature=0.07
 css_beta=0.5
 
 # ============ Training ============= #
-batch_size=3
+batch_size=1
 max_iter=20000
 # =================================== #
 
 
-# Run training script
-linear_text=""
-if $linear ; then
-  linear_text="--linear"
-fi
-
-styleclip=""
-if $styleclip_like ; then
-  styleclip="--styleclip"
-fi
-
+# ======= Run training script ======= #
 python train.py --gan=${gan} \
                 --truncation=0.7 \
                 --stylegan-space=${stylegan_space} \
+                --stylegan-layer=${stylegan_layer} \
                 --corpus=${corpus} \
                 --num-latent-support-dipoles=${num_latent_support_dipoles} \
                 --lss-beta=${lss_beta} \
+                --lr=1e-3 \
                 --loss=${loss} \
                 --temperature=${temperature} \
                 --css-beta=${css_beta} \
-                ${linear_text} \
-                ${styleclip} \
                 --min-shift-magnitude=${min_shift_magnitude} \
                 --max-shift-magnitude=${max_shift_magnitude} \
                 --batch-size=${batch_size} \
