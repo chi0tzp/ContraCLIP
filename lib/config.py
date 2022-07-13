fd823ea8a15ae8debdde94e0893c07da8af287bb
Christos Tzelepis <null.geppetto@gmail.com>
2022-07-13 21:00:11 +0100

    Modify attributes corpus

--- lib/config.py
+++ lib/config.py
@@ -13,114 +13,154 @@
 SEMANTIC_DIPOLES_CORPORA = {
     'attributes':
         [
+            # 0
             ["a photo of a female.",
              "a photo of a male."],
+            # 1
             ["a photo of an old person.",
              "a photo of a young person."],
-            ["a photo of a smiling person.",
-             "a photo of a sad person."],
+            # 2
+            ["a photo of a person with blonde hair.",
+             "a photo of a person with black hair"],
+            # 3
             ["a photo of a bald man.",
-             "a photo of a man with hair."],
+             "a photo of a man with long hair."],
+            # 4
             ["a photo of a man with beard.",
              "a photo of a shaved man."],
+            # 5
             ["a photo of a face with makeup.",
              "a photo of a face without makeup."],
-            ["a photo of a person with closed eyes.",
-             "a photo of a person with open eyes."],
-            ["a photo of a person with open lips.",
-             "a photo of a person with closed lips."],
+            # 6
             ["a photo of a person with tanned skin.",
              "a photo of a person with pale skin."],
         ],
     'expressions':
         [
+            # 0
             ["a photo of a person with a happy face.",
              "a photo of a person with a neutral face."],
+            # 1
             ["a photo of a person with a sad face.",
              "a photo of a person with a neutral face."],
+            # 2
             ["a photo of a person with a fearful face.",
              "a photo of a person with a neutral face."],
+            # 3
             ["a photo of a person with a disgusted face.",
              "a photo of a person with a neutral face."],
+            # 4
             ["a photo of a person with an angry face.",
              "a photo of a person with a neutral face."],
+            # 5
             ["a photo of a person in surprise.",
              "a photo of a person with a neutral face."],
+            # 6
             ["a photo of a person with a sad face.",
              "a photo of a person with a happy face."],
+            # 7
             ["a photo of a person with a fearful face.",
              "a photo of a person with a happy face."],
+            # 8
             ["a photo of a person with a disgusted face.",
              "a photo of a person with a happy face."],
+            # 9
             ["a photo of a person with an angry face.",
              "a photo of a person with a happy face."],
+            # 10
             ["a photo of a person in surprise.",
              "a photo of a person with a happy face."],
+            # 11
             ["a photo of a person with a fearful face.",
              "a photo of a person with a sad face."],
+            # 12
             ["a photo of a person with a disgusted face.",
              "a photo of a person with a sad face."],
+            # 13
             ["a photo of a person with an angry face.",
              "a photo of a person with a sad face."],
+            # 14
             ["a photo of a person in surprise.",
              "a photo of a person with a sad face."],
+            # 15
             ["a photo of a person with a disgusted face.",
              "a photo of a person with a fearful face."],
+            # 16
             ["a photo of a person with an angry face.",
              "a photo of a person with a fearful face."],
+            # 17
             ["a photo of a person in surprise.",
              "a photo of a person with a fearful face."],
+            # 18
             ["a photo of a person with an angry face.",
              "a photo of a person with a disgusted face."],
+            # 19
             ["a photo of a person in surprise.",
              "a photo of a person with a disgusted face."],
+            # 20
             ["a photo of a person in surprise.",
              "a photo of a person with an angry face."],
         ],
     'expressions3':
         [
+            # 0
             ["a photo of a person with a happy face.",
              "a photo of a person with an angry face."],
+            # 1
             ["a photo of a person in surprise.",
              "a photo of a person with an angry face."],
+            # 2
             ["a photo of a person in surprise.",
              "a photo of a person with a happy face."],
         ],
     'complex':
         [
+            # 0
             ["a photo of a man with a beard crying.",
              "a photo of an angry shaved man."],
+            # 1
             ["a photo of a man with a beard crying.",
              "a photo of a happy shaved man."],
+            # 2
             ["a photo of a man with a beard crying.",
              "a photo of a shaved man with makeup."],
         ],
     'dogs':
         [
+            # 0
             ["a photo of a happy dog.",
              "a photo of a sad dog."],
+            # 1
             ["a photo of a long haired dog.",
              "a photo of a short haired dog."],
+            # 2
             ["a photo of a friendly dog.",
              "a photo of an aggressive dog."],
+            # 3
             ["a photo of a dog with big eyes.",
              "a photo of a dog with small eyes."]
         ],
     'cats':
         [
+            # 0
             ["a photo of a long haired cat.",
              "a photo of a short haired cat."],
+            # 1
             ["a photo of a cute cat.",
              "a photo of an ugly cat."],
+            # 2
             ["a photo of a cat with big ears.",
              "a photo of a cat with small ears."]
         ],
     'cars':
         [
+            # 0
             ["a photo of a jeep.",
              "a photo of a low car."],
+            # 1
             ["a photo of a sports car.",
              "a photo of a city car."],
+            # 2
             ["a photo of a modern car.",
              "a photo of a car from the sixties."],
         ],
