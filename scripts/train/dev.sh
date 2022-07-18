0efdaacb2369eeb5fe56bc90324eb6cecdcf437a
Christos Tzelepis <null.geppetto@gmail.com>
2022-07-18 14:54:50 +0100

    WIP css gamma init

--- scripts/train/dev.sh
+++ scripts/train/dev.sh
@@ -13,26 +13,23 @@ corpus="expressions3"
 num_latent_support_dipoles=32
 min_shift_magnitude=0.1
 max_shift_magnitude=0.2
-lss_beta=0.5
 lr=1e-3
 
 # ==== Corpus Support Sets (CSS) ==== #
 loss="cossim"
 temperature=0.07
-css_beta=0.5
-css_learn_gammas=true
+id=false
 lambda_id=10000
 
 # ============ Training ============= #
-batch_size=3
-max_iter=1000
+batch_size=1
+max_iter=10000
 # =================================== #
 
 # Run training script
-css_learn_gammas_=""
-if $css_learn_gammas ; then
-  css_learn_gammas_="--css-learn-gammas"
-
+id_=""
+if $id ; then
+  id_="--id"
 fi
 
 # ======= Run training script ======= #
@@ -42,13 +39,11 @@ python train.py --gan=${gan} \
                 --stylegan-layer=${stylegan_layer} \
                 --corpus=${corpus} \
                 --num-latent-support-dipoles=${num_latent_support_dipoles} \
-                --lss-beta=${lss_beta} \
                 --lr=${lr} \
                 --loss=${loss} \
                 --lambda-id=${lambda_id} \
-                ${css_learn_gammas_} \
+                ${id_} \
                 --temperature=${temperature} \
-                --css-beta=${css_beta} \
                 --min-shift-magnitude=${min_shift_magnitude} \
                 --max-shift-magnitude=${max_shift_magnitude} \
                 --batch-size=${batch_size} \
