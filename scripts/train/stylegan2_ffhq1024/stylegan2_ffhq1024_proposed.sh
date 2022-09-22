5134d2d19ddca39a961bee2f8e3826b445e25791
Christos Tzelepis <null.geppetto@gmail.com>
2022-09-22 03:22:13 +0300

    Update training scripts

--- scripts/train/stylegan2_ffhq1024/stylegan2_ffhq1024_proposed.sh
+++ scripts/train/stylegan2_ffhq1024/stylegan2_ffhq1024_proposed.sh
@@ -4,33 +4,34 @@
 # ==================================================================================================================== #
 
 # === GAN Type / Corpus ============================================================================================== #
-gan="stylegan2_ffhq1024"        # Choose GAN type from lib/config.py:GENFORCE_MODELS
-stylegan_space="W+"             # Choose StyleGAN latent space: Z, W, W+, or S
-stylegan_layer=6                # In the case of W+ space, choose up to which layer to use for learning latent paths
-corpus="attributes-expressions" # Choose corpus of semantic dipoles from lib/config.py:SEMANTIC_DIPOLES_CORPORA
-vl_paths="geodesic"             # Choose type of VL paths ("non-geodesic" or "geodesic")
+gan="stylegan2_ffhq1024"             # Choose GAN type from lib/config.py:GENFORCE_MODELS
+stylegan_space="W+"                  # Choose StyleGAN latent space: Z, W, W+, or S
+stylegan_layer=10                    # In the case of W+ space, choose up to which layer to use for learning latent paths
+corpus="attributes-expressions"      # Choose corpus of semantic dipoles from lib/config.py:SEMANTIC_DIPOLES_CORPORA
+vl_paths="proposed"                  # Choose type of VL paths ("standard" or "proposed")
+# vl_paths="standard"
 
-# ==== Corpus Support Sets (CSS) ===================================================================================== #
+
+# ====[ Vision-Language Sphere Warper (VLSW) ========================================================================= #
 id=true                        # Impose ID preservation using ArcFace
-lambda_id=5e3                  # ID preservation loss weighting parameter
-gammas="diag"
-gamma_0=1.0
-learn_gammas=false             # Optimise CSS RBFs' gammas
-lambda_x=100.0
-temperature=1.0               # Contrastive loss temperature
+lambda_id=1e4                  # ID preservation loss weighting parameter
+gammas="diag"                  # CSS RBFs' gammas type ('diag' or 'spherical')
+gamma_0=1e-3                   # TODO: +++
+learn_gammas=true              # Optimise CSS RBFs' gammas
+temperature=0.09               # Contrastive loss temperature
 
 
-# ==== Latent Support Sets (LSS) ===================================================================================== #
-num_latent_support_dipoles=128 # Set number of support dipoles per support set in the GAN's latent space
-min_shift_magnitude=0.2        # set minimum latent shift magnitude
-max_shift_magnitude=0.4        # set maximum latent shift magnitude
+# ====[ Latent Mapper (LM) ]========================================================================================== #
+num_latent_support_dipoles=1   # Set number of support dipoles per support set in the GAN's latent space
+min_shift_magnitude=0.1        # set minimum latent shift magnitude
+max_shift_magnitude=0.2        # set maximum latent shift magnitude
 
 # === Training ======================================================================================================= #
 batch_size=6                   # Set training batch size (cannot be larger than the size of the given corpus)
-max_iter=10000                  # Set maximum number of training iterations
-lr=1e-2                        # set learning rate for learning the latent support sets LSS (with Adam optimizer)
-# ==================================================================================================================== #
+max_iter=5000                  # Set maximum number of training iterations
+lr=1e-1                        # set learning rate for learning the latent support sets LSS (with Adam optimizer)
 
+# ==================================================================================================================== #
 
 learn_gammas_=""
 if $learn_gammas ; then
@@ -56,7 +57,6 @@ python train.py --gan=${gan} \
                 --temperature=${temperature} \
                 ${id_} \
                 --lambda-id=${lambda_id} \
-                --lambda-x=${lambda_x} \
                 --lr=${lr} \
                 --min-shift-magnitude=${min_shift_magnitude} \
                 --max-shift-magnitude=${max_shift_magnitude} \
