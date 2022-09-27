8ad4c9f8a37e960b43614399e2491248a75669d8
Christos Tzelepis <null.geppetto@gmail.com>
2022-09-27 13:54:01 +0300

    add training scripts

--- scripts/train/stylegan2_ffhq1024/stylegan2_ffhq1024_expressions_standard.sh
+++ scripts/train/stylegan2_ffhq1024/stylegan2_ffhq1024_expressions_standard.sh
@@ -4,27 +4,27 @@
 # ==================================================================================================================== #
 
 # === GAN Type / Corpus ============================================================================================== #
-gan="stylegan2_ffhq1024"       # Choose GAN type from lib/config.py:GENFORCE_MODELS
-stylegan_space="W+"            # Choose StyleGAN latent space: Z, W, W+, or S
-stylegan_layer=9               # In the case of W+ space, choose up to which layer to use for learning latent paths
-corpus="expressions"           # Choose corpus of semantic dipoles from lib/config.py:SEMANTIC_DIPOLES_CORPORA
+gan="stylegan2_ffhq1024"         # Choose GAN type from lib/config.py:GENFORCE_MODELS
+stylegan_space="W+"              # Choose StyleGAN latent space: Z, W, W+, or S
+stylegan_layer=12                # In the case of W+ space, choose up to which layer to use for learning latent paths
+corpus="expressions"             # Choose corpus of semantic dipoles from lib/config.py:SEMANTIC_DIPOLES_CORPORA
 vl_sim="standard"
 
 # ====[ Vision-Language Sphere Warper (VLSW) ========================================================================= #
 include_cls_in_mean=true
 id=true                        # Impose ID preservation using ArcFace
 lambda_id=5e3                  # ID preservation loss weighting parameter
-gamma_0=1e0                    # TODO: +++
-temperature=0.07               # Contrastive loss temperature
+gamma_0=1e0
+temperature=0.01                # Contrastive loss temperature
 
-# ====[ Latent Mapper (LM) ]========================================================================================== #
+# ====[ Latent Support Sets (LSS) ]==================================================================================== #
 num_latent_support_dipoles=1   # Set number of support dipoles per support set in the GAN's latent space
 min_shift_magnitude=0.1        # set minimum latent shift magnitude
 max_shift_magnitude=0.2        # set maximum latent shift magnitude
 
 # === Training ======================================================================================================= #
 batch_size=3                   # Set training batch size (cannot be larger than the size of the given corpus)
-max_iter=3000                  # Set maximum number of training iterations
+max_iter=5000                  # Set maximum number of training iterations
 lr=1e-1                        # set learning rate for learning the latent support sets LSS (with Adam optimizer)
 # ==================================================================================================================== #
 
